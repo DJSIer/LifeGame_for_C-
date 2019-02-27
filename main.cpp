364a8eddb9be58f558c6c7eedb85a66e0dd02a81
DJSIer <djsier.dtm@gmail.com>
2019-02-27 10:34:26 +0900

    first

---
+++ main.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<stdio.h>
+#include<memory.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<time.h>
+
+using namespace std;
+
+//横の長さ
+const int WIDTH=16;
+//たての長さ
+const int HEIGHT=16;
+//ランダムの生成回数
+const int NUM = 50;
+
+//マップ
+int map[HEIGHT][WIDTH];
+//八方向ベクトル
+int dx[8]={1,-1,0,0,1,-1,1,-1};
+int dy[8]={0,0,1,-1,1,-1,-1,1};
+
+//初期化
+int init(){
+    /*for(int i=0;i<HEIGHT;i++){
+        for(int i2=0;i2<WIDTH;i2++){
+            map[i][i2]=0;
+        }
+    }*/
+    //上記とほぼ同じ
+    memset(map,0,sizeof(map));
+    for(int i=0;i<NUM;i++){
+         //map[rand()%HEIGHT][rand()%WIDTH]=1;
+    }
+    map[5][5]=1;
+    map[6][5]=1;
+    map[7][5]=1;
+    map[6][4]=1;
+
+    map[5][10]=1;
+    map[6][10]=1;
+    map[7][10]=1;
+    map[6][11]=1;
+}
+int check(){
+    int nextMap[HEIGHT][WIDTH];
+    memcpy(nextMap,map,sizeof(map));
+    for(int y=0;y<HEIGHT;y++){
+        for(int x=0;x<WIDTH;x++){
+            int count=0;
+            for(int i3=0;i3<8;i3++){
+                if(y+dy[i3]>=0&&y+dy[i3]<HEIGHT && x+dx[i3]>=0 && x + dx[i3]<WIDTH){
+                    if(map[y+dy[i3]][x+dx[i3]]){
+                        count++;
+                    }
+                }
+                switch (count)
+                {
+                    case 0:
+                        nextMap[y][x]=0;
+                    case 1:
+                        nextMap[y][x]=0;
+                        /* code */
+                        break;
+                    case 2:
+                        if(map[y][x]){
+                            nextMap[y][x]=1;
+                        }
+                        break;
+                    case 3:
+                        nextMap[y][x]=1;
+                        break;         
+                    default:
+                        nextMap[y][x]=0;
+                        break;
+                }
+            }
+        }
+    }
+     memcpy(map,nextMap,sizeof(nextMap));
+}
+
+//フィールド表示
+int print(){
+    printf("\e[2\e[2J");
+    for(int i=0;i<HEIGHT;i++){
+        printf("|");
+        for(int i2=0;i2<WIDTH;i2++){
+
+            if(map[i][i2]){
+                printf("■");
+            }
+            else{
+                printf("□");
+            }
+            printf("|");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+
+    int count=0;
+    srand((unsigned)time(NULL));
+    init();
+    while(1){
+
+        print();
+        usleep(500000);
+        check();
+    }
+}
